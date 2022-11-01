509752f14998f337236bcc8dcbae30747140a0ba
Chase Gaddis <cgaddis36@gmail.com>
2022-11-01 17:06:31 -0500

    add paths for experience, education & about

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css'
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Splash from './components/screens/splash'
+import Education from './components/screens/education'
+import Experience from './components/screens/experience'
+import About from './components/screens/about'
 
 function App() {
   return (
@@ -9,6 +12,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Splash/>}/>
+          <Route path="/education" element={<Education/>}/>
+          <Route path="/experience" element={<Experience/>}/>
+          <Route path="/about" element={<About/>}/>
+
         </Routes>
       </Router>
     </div>
