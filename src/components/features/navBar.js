25941e3acb17f26fccd56fa340ee4512800c56c0
Chase Gaddis <cgaddis36@gmail.com>
2022-11-01 17:06:55 -0500

    pass onClick function for path navigation

--- src/components/features/navBar.js
+++ src/components/features/navBar.js
@@ -1,23 +1,37 @@
 import React, {useState} from 'react'
 import styles from '../styles/navBar.module.css'
-
+import { useNavigate } from 'react-router-dom'
 function NavBar({
   active
 }) {
+  const navigate = useNavigate()
+  const swapPath = (path) => {
+    navigate(path)
+  }
   return (
   <div className={styles.nav}>
     <div 
+      onClick={() => swapPath('/')} 
       className={active === 'projects' ? styles.active : styles.navButton}
       >
       Projects
     </div>
-     <div className={styles.navButton}>
+     <div
+      onClick={() => swapPath('/education')} 
+      className={active === 'education' ? styles.active : styles.navButton}
+      >
       Education
     </div>
-    <div className={styles.navButton}>
+    <div 
+      onClick={() => swapPath('/experience')} 
+      className={active === 'experience' ? styles.active : styles.navButton}
+      >
       Experience
     </div>
-    <div className={styles.navButton}>
+    <div 
+      onClick={() => swapPath('/about')} 
+      className={active === 'about' ? styles.active : styles.navButton}
+      >
       About Me
     </div>
   </div>
