0b3e1779415c694e3ddf67fb82042ab2ad06c8b2
Chase Gaddis <cgaddis36@gmail.com>
2022-11-01 15:32:10 -0500

    modify loading animation add navBar

---
+++ src/components/screens/splash.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react'
+import FetchProjects from '../apiCalls/fetchProjects'
+import styles from '../styles/splash.module.css'
+import NavBar from '../features/navBar'
+import ProjectBox from '../features/projectBox' 
+
+function Splash() {
+  const [loading, setLoading] = useState(false)
+  const [projects, setProjects] = useState([])
+  useEffect(() => {
+    FetchProjects({
+      setLoading,
+      setProjects
+    })
+  }, [])
+  return (
+    <div className={styles.main}>
+      <div className={styles.title}>
+        Chase's Portfolio
+      </div>
+      <NavBar
+        active={'projects'}
+        />
+      <div className={styles.container}>
+        {
+          loading ?
+          <div className={styles.loading}>
+            <div className={styles.loading__letter}>L</div>
+            <div className={styles.loading__letter}>o</div>
+            <div className={styles.loading__letter}>a</div>
+            <div className={styles.loading__letter}>d</div>
+            <div className={styles.loading__letter}>i</div>
+            <div className={styles.loading__letter}>n</div>
+            <div className={styles.loading__letter}>g</div>
+            <div className={styles.loading__letter}>.</div>
+            <div className={styles.loading__letter}>.</div>
+            <div className={styles.loading__letter}>.</div>
+          </div>
+          :
+          projects.map((project, index) =>
+            <ProjectBox
+              key={index}
+              name={project.name}
+              description={project.description}
+              features={project.features}
+              apis={project.apis}
+              frameworks={project.frameworks}
+              classification={project.classification}
+              hosts={project.hosts}
+              url={project.url}
+              github={project.github}
+            />
+          )
+        }
+      </div>
+
+    </div>
+  )
+}
+export default Splash
