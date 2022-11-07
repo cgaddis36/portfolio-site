6e069c5cab12d863a8354573f537eaa9929d18b8
Chase Gaddis <cgaddis36@gmail.com>
2022-11-07 16:21:19 -0600

    add videos to hobby section of about page

--- src/components/screens/about.js
+++ src/components/screens/about.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/about.module.css'
 import Header from '../features/header'
 import beachSunset from '../images/beachSunset.png'
@@ -6,8 +6,37 @@ import skiing from '../images/skiChris.png'
 import flyfish from '../images/fly.png'
 import beachBikes from '../images/beachBikes.png'
 import hike from '../images/hike.png'
+import FlyVid from '../videos/beachFly.mp4'
+import SkiVid from '../videos/kachina.mp4'
+import DiveVid from '../videos/diving.mp4'
+import BikeVid from '../videos/biking.mp4'
+import HikeVid from '../videos/hiking.mp4'
+
 
 function About() {
+  const hobbies = [
+    {name: 'Fly Fishing', image: flyfish, video: FlyVid},
+    {name: 'Hiking', image: hike, video: HikeVid},
+    {name: 'Skiing', image: skiing, video: SkiVid},
+    {name: 'Diving', image: beachSunset, video: DiveVid},
+    {name: 'Biking', image: beachBikes, video: BikeVid},
+  ]
+  const [activeHobby, setActiveHobby] = useState(0)
+  const hobby = hobbies[activeHobby]
+  const activeVideo = hobby.video
+  const checkArrow = (direction) => {
+    if(direction == 'right') {
+      return(hobby.name !== 'Biking')
+    } else {
+      return(hobby.name !== 'Fly Fishing')
+    }
+  }
+  const backHobby = () => {
+    if(hobby.name !== 'Fly Fishing'){setActiveHobby(activeHobby - 1)}
+  }
+  const nextHobby = () => {
+    if(hobby.name !== 'Biking'){setActiveHobby(activeHobby + 1)}
+  }
   return(
     <div className={styles.main}>
       <Header 
@@ -15,6 +44,8 @@ function About() {
         title={"About Me"}
         /> 
       <div className={styles.container}>
+        <video src={activeVideo} autoPlay muted loop className={styles.video}/>
+        <div className={styles.overlay}></div>
         <div className={styles.section}>
           <div className={styles.header}>
             Background
@@ -28,60 +59,40 @@ function About() {
           </div>
         </div>
         <div className={styles.section}>
-
           <div className={styles.header}>
             Hobbies
           </div>
-        <div className={styles.hobbies}>
-          <div className={styles.hobby}>
-            Fly Fishing
-            <div className={styles.images}>
-              <img 
-                src={flyfish}
-                className={styles.hobbyImage}
-                />
-            </div>
-          </div>
-          <div className={styles.hobby}>
-            Hiking
-            <div className={styles.images}>
-              <img 
-                src={hike}
-                className={styles.hobbyImage}
-                />
-            </div>
-          </div>
-          <div className={styles.hobby}>
-            Skiing
-            <div className={styles.images}>
-              <img 
-                src={skiing}
-                className={styles.hobbyImage}
-                />
-            </div>
-          </div>
-          
-          <div className={styles.hobby}>
-            Diving
-            <div className={styles.images}>
-              <img 
-                src={beachSunset}
-                className={styles.hobbyImage}
-                />
-            </div>
-          </div>
-          <div className={styles.hobby}>
-            Biking
-            <div className={styles.images}>
-              <img 
-                src={beachBikes}
-                className={styles.hobbyImage}
-                />
-            </div>
-          </div>
-        </div>
-        </div>
 
+          <div className={styles.hobbies}>
+            <div className={styles.images}>
+              <img 
+                src={hobby.image}
+                className={styles.hobbyImage}
+                />
+            </div>
+            <div className={styles.column}>
+              <div className={styles.hobby}>
+                {hobby.name}
+              </div>
+              <div className={styles.nextButtons}>
+               <div 
+                  src={leftArrow}
+                  onClick={() => backHobby()}
+                  className={checkArrow('left') ? styles.activeArrow : styles.inactiveArrow}
+                  > 
+                  Last Hobby
+                  </div>
+                <div 
+                  src={rightArrow}
+                  onClick={() => nextHobby()}
+                  className={checkArrow('right') ? styles.activeArrow : styles.inactiveArrow}
+                  >
+                    Next Hobby
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
