6e759aa751905b971483e23c7147a9e92f17ea8c
Chase Gaddis <cgaddis36@gmail.com>
2022-12-16 16:40:55 -0600

    rearrange header

--- src/components/features/header.js
+++ src/components/features/header.js
@@ -1,39 +1,44 @@
 import React from 'react'
 import styles from '../styles/header.module.css'
 import NavBar from '../features/navBar'
+import { useNavigate } from 'react-router-dom'
 
 function Header({
   active,
   title
 }) {
+  const navigate = useNavigate()
+  const swapPath = (path) => {
+    navigate(path)
+  }
   return(
     <div className={styles.top}>
       <div className={styles.header}>
-        <div className={styles.headerContainer}>
-          <div className={styles.name}>
-            Chase Gaddis
-          </div>
-          <div className={styles.role}>
-            Software Engineer
-          </div>
-        
-        </div>
+        <NavBar
+          active={active}
+          />
         <div className={styles.title}>
           {title}
         </div>
         <div className={styles.headerContainer}>
-          <div className={styles.email}>
-              cgaddis36@gmail.com
+          <div className={styles.headerInfoColumn}>
+            <div className={styles.name}>
+              Chase Gaddis
+            </div>
+            <div className={styles.role}>
+              Software Engineer
+            </div>
           </div>
-          <div className={styles.info}>
-              (850)582-2223
-          </div>
+          <div className={styles.headerInfoColumn}>
+            <div className={styles.email}>
+                cgaddis36@gmail.com
+            </div>
+            <div className={styles.info}>
+                (850)582-2223
+            </div>
+           </div>
         </div>
-
       </div>
-      <NavBar
-          active={active}
-          />
     </div>
   )
 }
