53fcaa6b4f0cda4f97963513bd87e1d657d8ec11
Chase Gaddis <cgaddis36@gmail.com>
2024-12-16 18:56:35 -0500

    update imports for data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './App.css'
-import React, { useState } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Projects from './components/screens/projects'
 import Education from './components/screens/education'
 import Experience from './components/screens/experience'
 import About from './components/screens/about'
-import hobbies from './components/data/hobbies'
-
+import hobbies from './assets/data/hobbies.tsx'
+import institutions from './assets/data/institutions.tsx'
+import jobs from './assets/data/jobs.tsx'
+import projects from './assets/data/projects.tsx'
+import developer from './assets/data/developer'
 function App() {
-  const [loading, setLoading] = useState(true)
 
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<About hobbies={hobbies} loading={loading}/>}/>
-          <Route path="/education" element={<Education developer={developer}/>}/>
-          <Route path="/experience" element={<Experience developer={developer}/>}/>
-          <Route path="/projects" element={<Projects developer={developer}/>}/>
+          <Route path="/" element={<About developer={developer} hobbies={hobbies}/>}/>
+          <Route path="/education" element={<Education institutions={institutions}/>}/>
+          <Route path="/experience" element={<Experience jobs={jobs}/>}/>
+          <Route path="/projects" element={<Projects projects={projects}/>}/>
         </Routes>
       </Router>
     </div>
