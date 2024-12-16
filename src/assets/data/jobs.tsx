60d0d812cc6bd02d4c71acb89c05f86ef4ba720d
Chase Gaddis <cgaddis36@gmail.com>
2024-12-16 18:56:47 -0500

    create static data

---
+++ src/assets/data/jobs.tsx
@@ -0,0 +1,39 @@
+import Job from "../types/job";
+
+const jobs: Job[] = [
+  {
+    id: 1,
+    title: "Full-Stack Engineer, Co-Founder",
+    company: "Shrimp Republic Studios",
+    industry: "Software Development & Consulting",
+    startDate: "08/2021",
+    endDate: "08/2022",
+    city: "Palmetto",
+    state: "FL",
+    summary: ""
+  },
+  {
+    id: 2,
+    title: "Full-Stack Engineer",
+    company: 'LeapEasy',
+    industry: 'InsurTech',
+    startDate: '07/2021',
+    endDate: '08/2022',
+    city: 'Jersey City',
+    state: 'NJ',
+    summary: 'Developed an iOS application for recreational fishermen and boaters on AppleConnect, tested on Test Flight. Built separate application designed to help local charter boat captain book trips.'
+  },
+  {
+    id: 3,
+    title: 'Back-End Engineer',
+    company: 'Promenade.ai',
+    industry: 'Veteran Services',
+    startDate: '01/2021',
+    endDate: '04/2021',
+    city: 'Atlanta',
+    state: 'GA',
+    summary: "Designed & developed a backend microservice to help Veterans locate medical services in their area. Also built an admin dashboard for this application. Remote role."
+  },
+]
+
+export default jobs
