e558f4c737ed1c87f09b265fad8925636f480550
Chase <cgaddis36@gmail.com>
2024-12-17 10:03:14 -0500

    remove unused functions

--- src/components/features/header.js
+++ src/components/features/header.js
@@ -1,16 +1,10 @@
-import React from 'react'
 import styles from '../styles/header.module.css'
 import NavBar from '../features/navBar'
-import { useNavigate } from 'react-router-dom'
 
 function Header({
   active,
   title
 }) {
-  const navigate = useNavigate()
-  const swapPath = (path) => {
-    navigate(path)
-  }
   return(
     <div className={styles.top}>
       <div className={styles.header}>
@@ -26,15 +20,12 @@ function Header({
               Chase Gaddis
             </div>
             <div className={styles.role}>
-              Software Engineer
+              Full-Stack Engineer
             </div>
           </div>
           <div className={styles.headerInfoColumn}>
             <div className={styles.email}>
                 cgaddis36@gmail.com
-            </div>
-            <div className={styles.info}>
-                (850)582-2223
             </div>
            </div>
         </div>
