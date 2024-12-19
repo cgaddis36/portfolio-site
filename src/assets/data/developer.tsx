efb5ab819d92b4eeb262f833b6c93e02b702f941
Chase <cgaddis36@gmail.com>
2024-12-18 23:33:46 -0500

    modify description

--- src/assets/data/developer.tsx
+++ src/assets/data/developer.tsx
@@ -7,7 +7,7 @@ const developer: Developer = {
   email: "cgaddis36@gmail.com",
   github: "https://github.com/cgaddis36",
   title: "Software Developer",
-  background: "My main goal as a developer is to build highly personable applications designed to optimize efficiency and create solutions to real-world problems. Innovation & collaboration are what drive me in every project I build. Whether it be developing new services for startups, bringing my own ideas to life, or collaborating  for enterprise software, creating products to solve business problems and provide widespread accessibility to arbitrary data is what greases my gears as a developer. When I am not coding, you can find me throwing the ball with my dog in one of the local parks or fishing for inshore saltwater gamefish from a kayak. I would be hard-pressed to find a better day than hiking to a mountain stream and fly fishing for its entireity."
+  background: "My main goal as a developer is to build highly personable applications designed to optimize efficiency and create solutions to real-world problems. Innovation & collaboration are what drive me in every project I build. Whether it be developing new services for startups, bringing my own ideas to life, or collaborating on large team projects, developing solutions for products that solve business problems and provide widespread accessibility to arbitrary data is what truly greases my gears as a developer. When I am not coding, you can find me throwing the ball with my dog in one of the local parks, or fishing for inshore saltwater gamefish from a kayak. I would be hard-pressed to find a better day than hiking to a mountain stream and fly fishing all day."
 }
 
 export default developer
