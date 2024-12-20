83dd35d795d988c7eef06dc5253996444e268d31
Chase <cgaddis36@gmail.com>
2024-12-20 18:37:57 -0500

    update projects data

--- src/assets/data/projects.tsx
+++ src/assets/data/projects.tsx
@@ -3,165 +3,354 @@ const projects: Project[] = [
   {
     id: 1,
     name: 'ShoreCasts',
-    classification: 'Personal Project',
+    classification: 'Mobile App',
     url: 'https://www.shorecasts.com',
-    frameworks: ['Rails', 'GraphQL', 'React'],
-    languages: ['ruby', 'javascript', 'psql'],
-    hosts: ['AWS Amplify', 'Elastic Beanstalk', 'RDS database'],
-    description: 'Designed to centralize the location for all of the necessary forecasting tools that recreational boaters and saltwater anglers utilize to plan their days on the water.',
-    github: 'https://www.shorecasts.com',
-    features: ['User Login', 
-    'Fishing Store Locator', 
-    'Fishing Guides Locator', 
-    'Hourly Weather Predictions', 
-    'Daily Weather Predictions', 
-    'Wind Forecast', 
-    'Water Temperature recordings', 
-    'Tidal Height Predictions', 
-    'Mailer'],
-    apis: ['NOAA Tides & Currents', 'National Weather Service', 'Custom API built designed for Fishing Stores & Guides'],
+    frameworks: [
+      'Rails', 
+      'React',
+      'React Native'
+    ],
+    languages: [
+      'Ruby', 
+      'JavaScript', 
+      'SQL', 
+      'GraphQL', 
+      'TypeScript'
+    ],
+    hosts: [
+      'Apple App Store',
+      'Google Play Store', 
+      'Elastic Beanstalk', 
+      'RDS',
+      'EC2',
+      'Route 53',
+      'Amplify'
+    ],
+    description: 'Mobile application designed to help inshore saltwater anglers Catch the Perfect Day by centralizing the necessary forecasting tools that recreational boaters and saltwater anglers utilize to plan their days on the water.',
+    github: 'private',
+    features: [
+      'Tidal Forecasts',
+      'Hourly Weather Predictions', 
+      'Daily Weather Predictions', 
+      'Water Temperatures', 
+      'Tackle Shop Locations',
+      'Marina Locations', 
+      'Wind Forecasts', 
+    ],
+    apis: [
+      'NOAA Tides & Currents', 
+      'National Weather Service', 
+    ],
   },
   {
     id: 2,
-    name: 'Shrimp Republic Flagship',
-    classification: 'Personal Project',
-    url: 'https://www.shorecasts.com',
-    frameworks: ['Rails', 'GraphQL', 'React'],
-    languages: ['ruby', 'javascript', 'psql'],
-    hosts: ['AWS Amplify', 'Elastic Beanstalk', 'RDS database'],
-    description: 'Designed to centralize the location for all of the necessary forecasting tools that recreational boaters and saltwater anglers utilize to plan their days on the water.',
-    github: 'https://www.shorecasts.com',
-    features: ['User Login', 
-    'Fishing Store Locator', 
-    'Fishing Guides Locator', 
-    'Hourly Weather Predictions', 
-    'Daily Weather Predictions', 
-    'Wind Forecast', 
-    'Water Temperature recordings', 
-    'Tidal Height Predictions', 
-    'Mailer'],
-    apis: ['NOAA Tides & Currents', 'National Weather Service', 'Custom API built designed for Fishing Stores & Guides'],
+    name: 'Shrimp Republic',
+    classification: 'Web App',
+    url: 'https://www.shrimp-republic.com',
+    frameworks: [
+      'Rails', 
+      'React'
+    ],
+    languages: [
+      'JavaScript', 
+      'GraphQL'
+    ],
+    hosts: [
+      'AWS Amplify'
+    ],
+    description: 'Flagship website for Shrimp Republic Studios designed to showcase development projects and general business information.',
+    github: 'private',
+    features: [
+      'Progressive Web Application', 
+      'Custom Artwork using Adobe Illustrator'
+    ],
+    apis: [],
   },
   {
     id: 3,
     name: 'GameDrive',
-    classification: 'Personal Project',
-    url: 'https://www.shorecasts.com',
-    frameworks: ['Rails', 'GraphQL', 'React'],
-    languages: ['ruby', 'javascript', 'psql'],
-    hosts: ['AWS Amplify', 'Elastic Beanstalk', 'RDS database'],
+    classification: 'Web App',
+    url: 'http://gamedev.ucode.run/auth/sign-in',
+    frameworks: [
+      'Vue', 
+    ],
+    languages: [
+      'GoLang', 
+      'JavaScript', 
+      'SQL'
+    ],
+    hosts: [
+      'AWS Amplify', 
+      'Elastic Beanstalk', 
+      'RDS'
+    ],
     description: 'Designed to centralize the location for all of the necessary forecasting tools that recreational boaters and saltwater anglers utilize to plan their days on the water.',
-    github: 'https://www.shorecasts.com',
-    features: ['User Login', 
-    'Fishing Store Locator', 
-    'Fishing Guides Locator', 
-    'Hourly Weather Predictions', 
-    'Daily Weather Predictions', 
-    'Wind Forecast', 
-    'Water Temperature recordings', 
-    'Tidal Height Predictions', 
-    'Mailer'],
-    apis: ['NOAA Tides & Currents', 'National Weather Service', 'Custom API built designed for Fishing Stores & Guides'],
+    github: 'private',
+    features: [
+      'User Login', 
+      'Game Stats Tracker',
+      'Teams Dashboard'
+    ],
+    apis: [
+      'Steam', 
+      'Riot Games', 
+    ],
   },
   {
     id: 4,
     name: 'ChessMeito',
-    classification: 'Personal Project',
+    classification: 'Mobile App',
     url: 'https://www.shorecasts.com',
-    frameworks: ['Rails', 'GraphQL', 'React'],
-    languages: ['ruby', 'javascript', 'psql'],
-    hosts: ['AWS Amplify', 'Elastic Beanstalk', 'RDS database'],
-    description: 'Designed to centralize the location for all of the necessary forecasting tools that recreational boaters and saltwater anglers utilize to plan their days on the water.',
-    github: 'https://www.shorecasts.com',
-    features: ['User Login', 
-    'Fishing Store Locator', 
-    'Fishing Guides Locator', 
-    'Hourly Weather Predictions', 
-    'Daily Weather Predictions', 
-    'Wind Forecast', 
-    'Water Temperature recordings', 
-    'Tidal Height Predictions', 
-    'Mailer'],
-    apis: ['NOAA Tides & Currents', 'National Weather Service', 'Custom API built designed for Fishing Stores & Guides'],
+    frameworks: [
+      'Django', 
+      'React Native', 
+      'Vue'
+    ],
+    languages: [
+      'Python', 
+      'JavaScript', 
+      'SQL'
+    ],
+    hosts: [
+      'AWS Amplify', 
+      'Elastic Beanstalk', 
+      'Apple App Store',
+      'Google Play Store'
+    ],
+    description: 'ChessMeito, where chess meets history. Enjoy two curated puzzles a day with the free version or subscribe and dive into over 6,000 puzzles drawn from actual Grandmaster matches, putting you in the shoes of chess legends.',
+    github: 'private',
+    features: [
+      'Chess Games',
+      'User Subscriptions' 
+  ],
+    apis: [
+      'Glassfy'
+    ],
   },
   {
     id: 5,
     name: 'Claims Form',
-    classification: 'Work Project',
+    classification: 'Web App',
     url: 'https://www.leapeasy.com/claim-form/',
     github: 'https://www.leapeasy.com/claim-form/',
-    frameworks: ['Rails', 'GraphQL', 'React'],
-    languages: ['ruby', 'javascript', 'psql'],
-    hosts: ['AWS Amplify', 'Elastic Beanstalk', 'RDS database'],
-    description: 'Claims form designed to help landlords that work with Leap file insurance claims on their policies.',
-    apis: ['Salesforce'],
-    features: ['Google Places Autocomplete', 'Salesforce Automation', 'Salesforce Record Mutations'],
+    frameworks: [
+      'Rails', 
+      'GraphQL', 
+      'React'
+    ],
+    languages: [
+      'Ruby', 
+      'JavaScript', 
+      'SQL'
+
+    ],
+    hosts: [
+      'AWS Amplify', 
+      'Elastic Beanstalk', 
+      'RDS'
+
+    ],
+    description: 'Claims form designed to help landlords file insurance claims on their policies.',
+    apis: [
+      'Salesforce'
+
+    ],
+    features: [
+      'Google Places Autocomplete', 
+      'Salesforce Automation', 
+    ],
   },
   {
     id: 6,
     name: 'Lease Deposit Replacement Short Form',
-    classification: 'Work Project',
-    url: 'https://www.leapeasy.com/claim-form/',
-    github: 'https://www.leapeasy.com/claim-form/',
-    frameworks: ['Rails', 'GraphQL', 'React'],
-    languages: ['ruby', 'javascript', 'psql'],
-    hosts: ['AWS Amplify', 'Elastic Beanstalk', 'RDS database'],
-    description: 'Claims form designed to help landlords that work with Leap file insurance claims on their policies.',
-    apis: ['Salesforce'],
-    features: ['Google Places Autocomplete', 'Salesforce Automation', 'Salesforce Record Mutations'],
+    classification: 'Web App',
+    url: 'https://www.leapeasy.com/renter-ldr-form/',
+    github: 'Private',
+    frameworks: [
+      'Rails', 
+      'GraphQL', 
+      'React'
+    ],
+    languages: [
+      'Ruby', 
+      'JavaScript', 
+      'SQL'
+
+    ],
+    hosts: [
+      'AWS Amplify', 
+      'Elastic Beanstalk', 
+      'RDS'
+    ],
+    description: 'Short application form designed to screen tenants for insurance policies.',
+    apis: [
+      'Salesforce',
+      'Google Places',
+    ],
+    features: [
+      'Google Places Autocomplete', 
+      'Salesforce Automation', 
+    ],
   },
   {
     id: 7,
     name: 'Lease Deposit-Replacement Form',
-    classification: 'Work Project',
-    url: 'https://www.leapeasy.com/claim-form/',
+    classification: 'Web App',
+    url: 'https://www.leapeasy.com/renter-ldr-form/',
     github: 'https://www.leapeasy.com/claim-form/',
-    frameworks: ['Rails', 'GraphQL', 'React'],
-    languages: ['ruby', 'javascript', 'psql'],
-    hosts: ['AWS Amplify', 'Elastic Beanstalk', 'RDS database'],
-    description: 'Claims form designed to help landlords that work with Leap file insurance claims on their policies.',
-    apis: ['Salesforce'],
-    features: ['Google Places Autocomplete', 'Salesforce API Integration'],
+    frameworks: [
+      'Rails', 
+      'GraphQL', 
+      'React'
+    ],
+    languages: [
+      'Ruby', 
+      'JavaScript', 
+      'SQL'
+
+    ],
+    hosts: [
+      'AWS Amplify', 
+      'Elastic Beanstalk', 
+      'RDS'
+
+    ],
+    description: 'Application form designed to screen tenants for insurance policies.',
+    apis: [
+      'Salesforce'
+
+    ],
+    features: [
+      'Google Places Autocomplete', 
+      'Salesforce API Integration'
+    ],
   },
   {
     id: 8,
-    name: 'Landlord Contact Form',
-    classification: 'Work Project',
-    url: 'https://www.leapeasy.com/claim-form/',
-    github: 'https://www.leapeasy.com/claim-form/',
-    frameworks: ['Rails', 'GraphQL', 'React'],
-    languages: ['ruby', 'javascript', 'psql'],
-    hosts: ['AWS Amplify', 'Elastic Beanstalk', 'RDS database'],
-    description: 'Claims form designed to help landlords that work with Leap file insurance claims on their policies.',
-    apis: ['Salesforce'],
-    features: ['Google Places Autocomplete', 'Salesforce Automation', 'Salesforce Record Mutations'],
+    name: 'Rent Guaranty Form',
+    classification: 'Web App',
+    url: 'https://www.leapeasy.com/renter-application/',
+    github: 'Private',
+    frameworks: [
+      'Rails', 
+      'GraphQL', 
+      'React'
+    ],
+    languages: [
+      'Ruby', 
+      'JavaScript', 
+      'SQL'
+    ],
+    hosts: [
+      'AWS Amplify', 
+      'Elastic Beanstalk', 
+      'RDS'
+    ],
+    description: 'Application form designed to screen tenants for insurance policies.',
+    apis: [
+      'Salesforce'
+
+    ],
+    features: [
+      'Google Places Autocomplete', 
+      'Salesforce API Integration'
+    ],
   },
   {
     id: 9,
-    name: 'Landlord Invite Form',
-    classification: 'Work Project',
-    url: 'https://www.leapeasy.com/claim-form/',
-    github: 'https://www.leapeasy.com/claim-form/',
-    frameworks: ['Rails', 'GraphQL', 'React'],
-    languages: ['ruby', 'javascript', 'psql'],
-    hosts: ['AWS Amplify', 'Elastic Beanstalk', 'RDS database'],
-    description: 'Claims form designed to help landlords that work with Leap file insurance claims on their policies.',
-    apis: ['Salesforce'],
-    features: ['Google Places Autocomplete', 'Salesforce Automation', 'Salesforce Record Mutations'],
+    name: 'Landlord Contact Form',
+    classification: 'Web App',
+    url: 'https://www.leapeasy.com/landlord-application/',
+    github: 'Private',
+    frameworks: [
+      'Rails', 
+      'GraphQL', 
+      'React'
+    ],
+    languages: [
+      'Ruby', 
+      'JavaScript', 
+      'SQL'
+
+    ],
+    hosts: [
+      'AWS Amplify', 
+      'Elastic Beanstalk', 
+      'RDS'
+    ],
+    description: 'Claims form designed to help landlords sign up to offer a specific insurance product.',
+    apis: [
+      'Salesforce',
+      'Google Places'
+    ],
+    features: [
+      'Google Places Autocomplete', 
+      'Salesforce Automation', 
+    ],
   },
   {
     id: 10,
+    name: 'Landlord Invite Form',
+    classification: 'Web App',
+    url: 'N/A',
+    github: 'Private',
+    frameworks: [
+      'Rails', 
+      'GraphQL', 
+      'React'
+    ],
+    languages: [
+      'Ruby', 
+      'JavaScript', 
+      'SQL'
+
+    ],
+    hosts: [
+      'AWS Amplify', 
+      'Elastic Beanstalk', 
+      'RDS'
+
+    ],
+    description: 'Application form designed to help landlords sign their tenants up for insurance products.',
+    apis: [
+      'Salesforce',
+      'Google Places'
+    ],
+    features: [
+      'Google Places Autocomplete', 
+      'Salesforce Automation', 
+
+    ],
+  },
+  {
+    id: 11,
     name: 'Veterans to Doctors',
-    classification: 'Work Project',
+    classification: 'Web App',
     url: 'https://promenade.ai',
     github: 'https://promendae.ai',
-    hosts: ['Heroku'],
-    frameworks: ['Rails', 'GraphQL', 'React'],
-    languages: ['ruby', 'javascript', 'psql'],
-    apis: ['Constructed Api'],
-    features: ['Admin Dashboard', 
+    hosts: [
+      'Heroku'
+
+    ],
+    frameworks: [
+      'Rails', 
+      'GraphQL', 
+      'React'
+    ],
+    languages: [
+      'Ruby', 
+      'JavaScript', 
+      'SQL'
+    ],
+    apis: [
+      'Constructed Api'
+
+    ],
+    features: [
+      'Admin Dashboard', 
       'Custom Location Finder', 
-      'Stripe Payment Checkout'],
+      'Stripe Payment Checkout'
+    ],
       description: 'Microservice designed to help veterans locate specific medical services to facilitate their navigation of post-military life.'
   }
 ]
